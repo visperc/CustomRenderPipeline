d7ad9087c9e0b74d3ee897bf60321995a95a3a58
dumbo <visperc@gmail.com>
2020-12-03 18:16:26 +0800

    【功能添加】 初始化仓库

---
+++ CustomRP/Assets/CustomRP/CustomRenderPiplineAsset.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Visperc.CRP
+{
+    [CreateAssetMenu(menuName = "Rending/Custom Render Pipeline")]
+    public class CustomRenderPiplineAsset : RenderPipelineAsset
+    {
+        protected override RenderPipeline CreatePipeline()
+        {
+            return new CustomRenderPipeline();
+        }
+    }
+}
+
---
+++ CustomRP/Assets/CustomRP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Visperc.CRP
+{
+    public class CustomRenderPipeline : RenderPipeline
+    {
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        {
+
+            //throw new System.NotImplementedException();
+        }
+    }
+
+}
